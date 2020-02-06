98e429e9e5f876897ebc0d56f0a48ce4ad01207d
Gutierrez <brandongutierrez@my.whccd.edu>
2020-02-05 21:14:21 -0800

    Finished last question

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log(min(0,-10));
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-const circumference = (diameter) => {
+const circumference = diameter => {
     const pi = 3.14;
     return pi * diameter;
 }
@@ -24,3 +24,19 @@ console.log(circumference(5));
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+const searchString = (letter, word) => {
+
+  let numberCount = 0;
+    for (let i = 0; i <= word.length -1; i++)
+        if(numberCount[i] == letter) {
+            numberCount++;
+        }
+        return numberCount;
+};
+console.log(searchString("s", "mississippi"));
+
+/* 
+Easy way out: 
+let string = "mississippi";
+console.log(string.[1]); // I
+*/
